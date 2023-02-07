93b4050e42f715a76b556b2e4fc53e5a6a40adf2
jesusvela557 <jesusvela557>
2023-02-07 13:42:59 -0700

    display data from picture

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
 import Timer from './Components/Timer'; 
 import Header from './Components/Navbar';
-import read from './Components/reader';
 
-import Ingredients from './Components/Database';
+
+
 import FileUploadCard from './Components/Upload';
-import Read from './Components/reader';
+
+import IngredientsList from './Components/Database';
 function App() {
   return (
     <div className="App">
       <Header />
       <FileUploadCard />
        <Timer />
-       <Ingredients />
-       <Read/>
+       <IngredientsList />
+
     </div>
   );
 }
--- src/Components/Database.js
+++ src/Components/Database.js
@@ -1,16 +1,24 @@
+import React, { useState, useEffect } from "react";
+import Tesseract from "tesseract.js";
 
+const IngredientsList = () => {
+  const [ingredients, setIngredients] = useState([]);
 
-const ingredientsList = ["Caffeine", "Tren", "TRT","Shrooms", "Adderal"]
+  useEffect(() => {
+    Tesseract.recognize("https://edlatimore.com/assets/images/posts/gorilla-mind-smooth-ingredients.png?_cchid=aba4ce2ea791e05fc7b9b97065b4d680", "eng")
+      .then(({ data: { text } }) => {
+        setIngredients(text.split("\n").map(str => str.trim()));
+      })
+      .catch(console.error);
+  }, []);
 
-
-function Ingredients() { 
-return (
+  return (
     <div>
-        {ingredientsList.map((item, index) =>(
-            <div key={index}>ingredient: {item}</div>
-        ))}
+      {ingredients.map((item, index) => (
+        <div key={index}>ingredient: {item}</div>
+      ))}
     </div>
-)
-}
+  );
+};
 
-export default Ingredients
+export default IngredientsList;
